ec19d89d14fb2ecbdf181930bc9ec959a7dd04ef
kaya <jepsonkaya@gmail.com>
2019-05-31 11:44:29 -0700

    add if statement to lifeExpectancy to determine if they have lived past expected age and return how many years past they have lived

--- src/functions.js
+++ src/functions.js
@@ -1,79 +1,83 @@
 export class Ages{
   constructor(birthdate){
     this.birthdate = birthdate;
-    }
-
-    get earthAge() {
-      return this.ageFinderEarth();
-    }
-
-    get mercuryAge(){
-      return this.ageFinderMercury();
-    }
-    //
-    get venusAge(){
-      return this.ageFinderVenus();
-    }
-
-    get marsAge(){
-      return this.ageFinderMars();
-    }
-
-    get jupiterAge(){
-      return this.ageFinderJupiter();
-    }
-
-    get maleLife(){
-      return this.lifeExpectencyMale();
-    }
-
-    get femaleLife(){
-      return this.lifeExpectencyFemale();
-    }
-
-    ageFinderEarth() {
-      // let birthdateInput = document.querySelector('input[type="date"]');
-      let birthdateInput = new Date(this.birthdate + ' 00:00:00');
-      // let birthdate = new Date(birthdateInput.value);
-      let birthdateMilli = birthdateInput.getTime();
-      let today = Date.now();
-      let ageMillis = today - birthdateMilli;
-      let age = new Date(ageMillis);
-      let ageUTC = Math.abs(age.getUTCFullYear() - 1970);
-      return (ageUTC);
-    }
-
-    ageFinderMercury() {
-      let mercuryAge = Math.floor(this.ageFinderEarth() / .24);
-      return (mercuryAge);
-    }
-
-    ageFinderVenus() {
-      let venusAge = Math.floor(this.ageFinderEarth() / .62);
-      return (venusAge);
-    }
-
-    ageFinderMars() {
-      let marsAge = Math.floor(this.ageFinderEarth() / 1.88);
-      return (marsAge);
-    }
-
-    ageFinderJupiter() {
-      let jupiterAge = Math.floor(this.ageFinderEarth() / 11.86);
-      return (jupiterAge);
-    }
-
-    lifeExpectencyMale() {
-      let earthAge = this.ageFinderEarth();
-      let lifeExpectencyMale = 79 - earthAge;
-      return (lifeExpectencyMale);
-    }
-
-    lifeExpectencyFemale() {
-      let earthAge = this.ageFinderEarth();
-      let lifeExpectencyFemale = 84 - earthAge;
-      return (lifeExpectencyFemale);
-    }
-
-
   }
+
+  get earthAge() {
+    return this.ageFinderEarth();
+  }
+
+  get mercuryAge(){
+    return this.ageFinderMercury();
+  }
+  //
+  get venusAge(){
+    return this.ageFinderVenus();
+  }
+
+  get marsAge(){
+    return this.ageFinderMars();
+  }
+
+  get jupiterAge(){
+    return this.ageFinderJupiter();
+  }
+
+  get maleLife(){
+    return this.lifeExpectencyMale();
+  }
+
+  get femaleLife(){
+    return this.lifeExpectencyFemale();
+  }
+
+  ageFinderEarth() {
+    // let birthdateInput = document.querySelector('input[type="date"]');
+    let birthdateInput = new Date(this.birthdate + ' 00:00:00');
+    // let birthdate = new Date(birthdateInput.value);
+    let birthdateMilli = birthdateInput.getTime();
+    let today = Date.now();
+    let ageMillis = today - birthdateMilli;
+    let age = new Date(ageMillis);
+    let ageUTC = Math.abs(age.getUTCFullYear() - 1970);
+    return (ageUTC);
+  }
+
+  ageFinderMercury() {
+    let mercuryAge = Math.floor(this.ageFinderEarth() / .24);
+    return (mercuryAge);
+  }
+
+  ageFinderVenus() {
+    let venusAge = Math.floor(this.ageFinderEarth() / .62);
+    return (venusAge);
+  }
+
+  ageFinderMars() {
+    let marsAge = Math.floor(this.ageFinderEarth() / 1.88);
+    return (marsAge);
+  }
+
+  ageFinderJupiter() {
+    let jupiterAge = Math.floor(this.ageFinderEarth() / 11.86);
+    return (jupiterAge);
+  }
+
+  lifeExpectencyMale() {
+    let earthAge = this.ageFinderEarth();
+    if (earthAge > 79) {
+      return (earthAge - 79);
+    } else {
+      return (79 - earthAge);
+    }
+  }
+
+  lifeExpectencyFemale() {
+    let earthAge = this.ageFinderEarth();
+    if (earthAge > 84) {
+      return (earthAge - 84);
+    } else {
+      return (84 - earthAge);
+    }
+  }
+}
