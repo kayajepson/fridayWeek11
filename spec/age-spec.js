631ab7c971b3b7cdeea922a4e0f0787fdd14f434
kaya <jepsonkaya@gmail.com>
2019-05-31 10:41:33 -0700

    work on adding constructor test but karma throwing 'no undef' errors

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,54 +1,54 @@
-import { ageFinderEarth } from './../src/functions.js';
-import { ageFinderMercury } from './../src/functions.js';
-import { ageFinderVenus } from './../src/functions.js';
-import { ageFinderMars } from './../src/functions.js';
-import { ageFinderJupiter } from './../src/functions.js';
-import { earthAge } from './../src/functions.js';
+// import { ageFinderEarth } from './../src/functions.js';
+// import { ageFinderMercury } from './../src/functions.js';
+// import { ageFinderVenus } from './../src/functions.js';
+// import { ageFinderMars } from './../src/functions.js';
+// import { ageFinderJupiter } from './../src/functions.js';
+import { Ages } from './../src/functions.js';
 
-describe('ageFinderEarth', function() {
-  it('should test if entering your birthday returns your Earth age', function() {
-    let expectedAge = 29;
-    let inputAge = ageFinderEarth();
-    expect(expectedAge).toEqual(inputAge);
-  });
-});
+// describe('ageFinderEarth', function() {
+//   it('should test if entering your birthday returns your Earth age', function() {
+//     let expectedAge = 29;
+//     let inputAge = ageFinderEarth();
+//     expect(expectedAge).toEqual(inputAge);
+//   });
+// });
+//
+// describe('ageFinderMercury', function() {
+//   it('should test if entering your birthday returns your Mercury age', function() {
+//     let expectedAge = 120;
+//     let inputAge = ageFinderMercury();
+//     expect(expectedAge).toEqual(inputAge);
+//   });
+// });
+//
+// describe('ageFinderVenus', function() {
+//   it('should test if entering your birthday returns your Venus age', function() {
+//     let expectedAge = 46;
+//     let inputAge = ageFinderVenus();
+//     expect(expectedAge).toEqual(inputAge);
+//   });
+// });
+//
+// describe('ageFinderMars', function() {
+//   it('should test if entering your birthday returns your Mars age', function() {
+//     let expectedAge = 15;
+//     let inputAge = ageFinderMars();
+//     expect(expectedAge).toEqual(inputAge);
+//   });
+// });
+//
+// describe('ageFinderJupiter', function() {
+//   it('should test if entering your birthday returns your Jupiter age', function() {
+//     let expectedAge = 2;
+//     let inputAge = ageFinderJupiter();
+//     expect(expectedAge).toEqual(inputAge);
+//   });
+// });
 
-describe('ageFinderMercury', function() {
-  it('should test if entering your birthday returns your Mercury age', function() {
-    let expectedAge = 120;
-    let inputAge = ageFinderMercury();
-    expect(expectedAge).toEqual(inputAge);
-  });
-});
-
-describe('ageFinderVenus', function() {
-  it('should test if entering your birthday returns your Venus age', function() {
-    let expectedAge = 46;
-    let inputAge = ageFinderVenus();
-    expect(expectedAge).toEqual(inputAge);
-  });
-});
-
-describe('ageFinderMars', function() {
-  it('should test if entering your birthday returns your Mars age', function() {
-    let expectedAge = 15;
-    let inputAge = ageFinderMars();
-    expect(expectedAge).toEqual(inputAge);
-  });
-});
-
-describe('ageFinderJupiter', function() {
-  it('should test if entering your birthday returns your Jupiter age', function() {
-    let expectedAge = 2;
-    let inputAge = ageFinderJupiter();
-    expect(expectedAge).toEqual(inputAge);
-  });
-});
-
-describe('earthAge', function() {
+describe('Ages', function() {
   it('output earth age via constructor', function() {
     let expectedAge = 29;
-    let inputAge = new earthAge('1990/02/01');
-    expect(expectedAge).toEqual(inputAge);
+    let inputAge = new Ages('1990/02/01');
+    expect(expectedAge).toEqual(inputAge.earthAge);
   });
 });
--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,32 @@
-export class earthAge{
+export class Ages{
   constructor(birthdate){
-    this.getAge = function ageFinderEarth() {
-      let birthdateInput = new Date(birthdate+'T00:00:00');
+    this.birthdate = birthdate;
+    }
+
+    get earthAge() {
+      return this.ageFinderEarth();
+    }
+
+    get mercuryAge(){
+      return ageFinderMercury();
+    }
+
+    get venusAge(){
+      return ageFinderVenus();
+    }
+
+    get marsAge(){
+      return ageFinderMars();
+    }
+
+    get jupiterAge(){
+      return ageFinderJupiter();
+    }
+
+    ageFinderEarth() {
+      // let birthdateInput = document.querySelector('input[type="date"]');
+      let birthdateInput = new Date(this.birthdate + 'T00:00:00');
+      // let birthdate = new Date(birthdateInput.value);
       let birthdateMilli = birthdateInput.getTime();
       let today = Date.now();
       let ageMillis = today - birthdateMilli;
@@ -9,45 +34,66 @@ export class earthAge{
       let ageUTC = Math.abs(age.getUTCFullYear() - 1970);
       return (ageUTC);
     }
+
+    ageFinderMercury() {
+      let marsAge = Math.floor(this.ageFinderEarth() / .24);
+      return (marsAge);
+    }
+
+    ageFinderVenus() {
+      let venusAge = Math.floor(this.ageFinderEarth() / .62);
+      return (venusAge);
+    }
+
+    ageFinderMars() {
+      let marsAge = Math.floor(this.ageFinderEarth() / 1.88);
+      return (marsAge);
+    }
+
+    ageFinderJupiter() {
+      let jupiterAge = Math.floor(this.ageFinderEarth() / 11.86);
+      return (jupiterAge);
+    }
   }
-}
+
+const test = new Age('1990-02-01');
 
 
 
 
-export function ageFinderEarth() {
-  // let birthdateInput = document.querySelector('input[type="date"]');
-  let birthdateInput = new Date('1990-02-17T00:00:00');
-  // let birthdate = new Date(birthdateInput.value);
-  let birthdateMilli = birthdateInput.getTime();
-  let today = Date.now();
-  let ageMillis = today - birthdateMilli;
-  let age = new Date(ageMillis);
-  let ageUTC = Math.abs(age.getUTCFullYear() - 1970);
-  return (ageUTC);
-}
-
+// export function ageFinderEarth() {
+//   // let birthdateInput = document.querySelector('input[type="date"]');
+//   let birthdateInput = new Date('1990-02-17T00:00:00');
+//   // let birthdate = new Date(birthdateInput.value);
+//   let birthdateMilli = birthdateInput.getTime();
+//   let today = Date.now();
+//   let ageMillis = today - birthdateMilli;
+//   let age = new Date(ageMillis);
+//   let ageUTC = Math.abs(age.getUTCFullYear() - 1970);
+//   return (ageUTC);
+// }
+//
+// // export function ageFinderMercury() {
+// //   let marsAge = Math.floor(ageFinderEarth() / .24);
+// //   return (marsAge);
+// // }
+//
 // export function ageFinderMercury() {
-//   let marsAge = Math.floor(ageFinderEarth() / .24);
+//   let marsAge = Math.floor(this.getAge / .24);
 //   return (marsAge);
 // }
-
-export function ageFinderMercury() {
-  let marsAge = Math.floor(this.getAge / .24);
-  return (marsAge);
-}
-
-export function ageFinderVenus() {
-  let venusAge = Math.floor(ageFinderEarth() / .62);
-  return (venusAge);
-}
-
-export function ageFinderMars() {
-  let marsAge = Math.floor(ageFinderEarth() / 1.88);
-  return (marsAge);
-}
-
-export function ageFinderJupiter() {
-  let jupiterAge = Math.floor(ageFinderEarth() / 11.86);
-  return (jupiterAge);
-}
+//
+// export function ageFinderVenus() {
+//   let venusAge = Math.floor(ageFinderEarth() / .62);
+//   return (venusAge);
+// }
+//
+// export function ageFinderMars() {
+//   let marsAge = Math.floor(ageFinderEarth() / 1.88);
+//   return (marsAge);
+// }
+//
+// export function ageFinderJupiter() {
+//   let jupiterAge = Math.floor(ageFinderEarth() / 11.86);
+//   return (jupiterAge);
+// }
